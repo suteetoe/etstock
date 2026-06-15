b75dd5507438aeb953f21478cca4ddcb25900d21
suteetoe <sutee.toe@gmail.com>
2026-06-15 10:42:31 +0700

    fist

---
+++ ETStock/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace ETStock.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ ETStock/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ETStock.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
